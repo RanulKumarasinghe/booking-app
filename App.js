04bd3f5bbde0d86e7322c9fe5a9e7eca31073b68
MINVY-779VKA49\Bengel <minderrx2@gmail.com>
2020-10-06 17:26:24 +0100

    Added react navigation dependancy and set up the home page to use this feature along side kitten_UI library

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './components/Pages/Home'
 
-const HomeScreen = () => (
-  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category='h1'>HOME</Text>
-  </Layout>
-);
 
-export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <Home />
-  </ApplicationProvider>
-);
+const Stack = createStackNavigator();
+
+//The stack navigator defaults to home.jsx file which uses also Kitten-UI
+//The screen that was navigated to (home.jsx) gets passed a parameter (navigator)
+//To use stack navigator and go to any other screen use this parameter
+//For example to go to login page use navigator.navigate('Name of screen')
+//Screen names can be seen in Stack.screen 'name' tag
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} options={{title:'Home'}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
--- components/Pages/Login.jsx
+++ components/Pages/Login.jsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function Login({ restaurants }) {
-
-  return (
-    <View>
-    </View>
+export default function Login({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Login Screen</Text>
+        <Button
+          title="Go back Home"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </View>
   );
 }
