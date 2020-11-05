8ded858cb06ef4a0c2d20d9fcdf57a3553a28286
Laurence Barnes <lb569@kent.ac.uk>
2020-11-05 13:16:32 +0000

    New page creation

--- src/scenes/Restaurant.jsx
+++ src/scenes/Restaurant.jsx
@@ -1,19 +1,52 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Button, Text, View } from "react-native";
+import { StyleSheet, Button, Text, View, Image } from "react-native";
 
-const Restaurant = (props) => {
+const Restaurant = props => {
   return (
     <View>
-        <Text>Menu</Text>
-
-        <Text>Info</Text>
-
-        <Text>Deals</Text>
-
-        <Text>Seating plan</Text>
+      <View>
+          <Text>Restaurant Name</Text>
+      </View>
+      <View>
+          <Image/> 
+          /**Self closing tag above, should the images be added into a constat folder? */
+      </View> 
+      <View>
+          <Text>Restaurant Info</Text>
+      </View>
+      <View>
+          <Image/> 
+          /**The image for the star system 
+          Could have staic image for star system?*/
+      </View>
+      <View>
+          <Text>
+            Selected info about the Restaurant
+            This needs to be updated
+          </Text>
+      </View>
+      <View>
+        <Text>Place holder for menu</Text>
+      </View>
+      <View>
+        <Button title="Make A Booking" onPress={() => {console.log()}}/>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+    },
+
+    font:{},
+    button:{},
+
+
+});
 
 export default Restaurant;
