2b32b7c883c1c59f37af78abc65009ed8af60aa2
Pedro Roque <pmm34@kent.ac.uk>
2020-11-09 16:38:20 +0000

    Added redux provider

--- App.js
+++ App.js
@@ -3,13 +3,23 @@ import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { AppNavigator } from './src/navigations/routes';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux'
 
-export default () => (
-  <>
+import restaurantReducer from '@/store/reducers/restaurants';
+
+const rootReducer = combineReducers({
+  meals: restaurantReducer,
+});
+
+
+const App = () => (
+  <Provider store={store}>
     <IconRegistry icons={EvaIconsPack}/>
     <ApplicationProvider {...eva} theme={eva.light}>
       <AppNavigator/>
     </ApplicationProvider>
-
-  </>
+  </Provider>
 );
+
+export default App;
---
+++ src/store/reducers/restaurants.js
@@ -0,0 +1,21 @@
+// import { MEALS } from ../
+
+const initialState = {
+  restaurants: [
+    {
+      id: 1,
+      name: 'MC'
+    },
+    {
+      id: 2,
+      name: 'Burger King'
+    }
+  ]
+}
+
+const mealsReducer = (state = initialState, action) => {
+
+  return state
+}
+
+export default mealsReducer;
