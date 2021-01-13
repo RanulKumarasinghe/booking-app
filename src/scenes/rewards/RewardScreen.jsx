9198ec3af20b527160a2d10ff7fd79912591b096
Laurence Barnes <lb569@kent.ac.uk>
2021-01-13 14:08:31 +0000

    Update to the page. Revamp start of points system

--- src/scenes/rewards/RewardScreen.jsx
+++ src/scenes/rewards/RewardScreen.jsx
@@ -7,41 +7,33 @@ import { useSelector } from 'react-redux';
 import { State } from "react-native-gesture-handler";
 
 const RewardScreen = (props) => {
-  const onAddRestaurant = () => props.navigation.navigate('Add Restaurant');
-//This needs additional logic for example  useSelector(state => state.rewards.points);
-//points come from the initial state in the reducer
-//const currentReward = useSelector(state => state.rewards);
 
-state = {
-  points: [],
-  currentPoints: [],
-  pointsUsed: [],
-}
+  //1. User inputs amount
+  //2. Submit amount
+  //3. A code is generated
+  //4. Code is submited 
+  //5. Points redeemed
+  const [money, setMoney] = useState(0);
 
-onPointsAdded =  (points, currentPoints, pointsUsed) =>{
-// should update the fields 
-}
-
-//
-  const [money, setMoney] = useState('');
-//The code genrated to update points
-  const [code, setCode] = useState('');
 //Holds points and adds
   const [points, updatePoints] = useState(0);
 
-  const  pointsFromState = useSelector(state => state.points)
-  // const usersCollection = firestore().collection('rewards');
+  const inPutMoney = 0;
+  const codeGen = '';
 
-  // const userDocument = firestore()
-  // .collection('rewards')
-  // .doc('0h2ypiz5X94hzGqGBp0e');
+  const genCode = () =>{
+    //Using Divides by a number and generates a code
+    inPutMoney = inPutMoney  
+  };
 
-  function addPoints(){  
-   updatePoints(points + 1000);
+
+  function addPoints(){
+    //Money is converted into points  
+   updatePoints(points + placeholder);
   //  firebase.firestore().collection('rewards')
   //   usersRef
   //       .doc(uid)
-  //       .set({user_id: something, points: points + 1000, code: '12341', status: 'good'})
+  //       .set({user_id: something, points: points + placeholder, code: '12341', status: 'good'})
   //       .then(() => {
   //           // props.navigation.navigate('Login')}
   //           props.navigation.navigate('Login', {user: data})
@@ -53,51 +45,9 @@ onPointsAdded =  (points, currentPoints, pointsUsed) =>{
 
   // update(field:  | FieldPath, value: any, moreFieldsAndValues: any[]): Promise<void>;
 
-  //One time read
-  // const user = await firestore()
-  // .collection('Users')
-  // .doc('ABC')
-  // .get();
 
-  function addMoney(){
-    setMoney();
-  }
-  // const generateCode = async () => {
-    // function makeid() {
-    //   var text = "";
-    //   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    
-    //   for (var i = 0; i < 5; i++)
-    //     text += possible.charAt(Math.floor(Math.random() * possible.length));
-    
-    //   return text;
-    // }
-    //Math.random().toString(36).substr(2, 5);
-    // console.log(makeid());
-  //   }
-
-  //   console.log(money)
-  // Should return a code
-  // }
-
-  // const redeemCode = async (code) => {
-  //   // Identify the inputs - code
-
-  //   console.log(code)
-   //Should say it's okay when points are added
-  // }
-
-  // firestore()
-  // .collection('Users')
-  // .get()
-  // .then(querySnapshot => {
-  //   console.log('Total users: ', querySnapshot.size);
-
-  //   querySnapshot.forEach(documentSnapshot => {
-  //     console.log('User ID: ', documentSnapshot.id, documentSnapshot.data());
-  //   });
-  // });
-
+ 
+  
   return (
     <SafeAreaView style={{ flex: 1 }}>
         <View style={styles.container}>
@@ -120,18 +70,32 @@ onPointsAdded =  (points, currentPoints, pointsUsed) =>{
           </View>
           <View>
             <Text style={styles.font}>Your Points:</Text>
-            <Text style={styles.font}>{points} {pointsFromState}</Text>
+            <Text style={styles.font}>{points}</Text>
           </View>
 
-          <View style={styles.button}><Button title="Use Points" onPress={() => { console.log('Use Points') }} /></View>
-          <View style={styles.button}><Button title="Refresh Points" onPress={() => { console.log('Refresh points') }} /></View>
-          <View style={styles.button}><Button title="Points History" onPress={() => { console.log('Loading History') }} /></View>
-          <View style={styles.button}><Button title="Add Restaurant" onPress={onAddRestaurant} /></View>
-        </View>
+          <View style={styles.button}>
+            <Button title="Use Points" 
+                    onPress={() => { console.log('Use Points') }} />
+            </View>
+            <View style={styles.button}>
+              <Button title="Refresh Points" 
+                    onPress={() => { console.log('Refresh points') }} />
+            </View>
+            <View style={styles.button}>
+              <Button title="Points History" 
+                    onPress={() => { console.log('Loading History') }} />
+            </View>
+          </View>
 
         <View>
           <TextInput
-            style={{ height: 40, borderColor: 'gray', borderWidth: 1, width:'80%', alignSelf:'center', alignContent:'center'}}
+            style={{ 
+              height: 40, 
+              borderColor: 'gray', 
+              borderWidth: 1, width:'80%', 
+              alignSelf:'center', 
+              alignContent:'center'
+            }}
             setMoney={price => setMoney(price)}
             value={money}
             keyboardType={'numeric'}
