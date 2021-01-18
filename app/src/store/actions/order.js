b7f9c25eaacc97ecf02602364eea73aede5cd9cb
Pedro Roque <pmm34@kent.ac.uk>
2021-01-18 16:42:51 +0000

    Added Action Store

---
+++ app/src/store/actions/order.js
@@ -0,0 +1,35 @@
+import Firebase from 'src/utils/firebase';
+export const NEW_ORDER = 'NEW_ORDER';
+
+export const newOrder = (restaurantId) => {
+    return async dispatch => {
+      dispatch({ type: NEW_ORDER, restaurantId: restaurantId})
+    } 
+};
+
+export const SET_ORDER_TYPE = 'SET_ORDER_TYPE';
+
+export const setOrderType = (type) =>{
+  return async dispatch => {
+    dispatch({ type: SET_ORDER_TYPE, type: type})
+  } 
+};
+
+export const SET_ITEM = 'SET_ITEM';
+
+export const setOrderType = (item) =>{
+  return async dispatch => {
+    dispatch({ type: SET_ITEM, item: item })
+  } 
+};
+
+
+export const REMOVE_ITEM = 'REMOVE_ITEM';
+
+export const setOrderType = (item) =>{
+  return async dispatch => {
+    dispatch({ type: REMOVE_ITEM, item: item })
+  } 
+};
+
+
---
+++ app/src/store/reducers/order.js
@@ -0,0 +1,26 @@
+import { NEW_ORDER, SET_ORDER_TYPE, SET_ITEM, REMOVE_ITEM } from '../actions/order';
+
+const initialState = {
+  cart: [],
+};
+
+const orderReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case NEW_ORDER:
+          //this action
+          return {restaurantId: action.restaurantId, cart: []};
+        case SET_ORDER_TYPE:
+          return {...state, type: action.type};
+        case SET_ITEM:
+          let newCart = state.cart.filter(cartItem => cartItem.id != action.item.id)
+          newCart.push(action.item)
+          return {...state, cart: newCart};
+        case REMOVE_ITEM:
+          let newCart = state.cart.filter(cartItem => cartItem.id != action.item.id)
+          return {...state, cart: newCart};
+        default:
+          return state;
+    }
+}
+
+export default orderReducer;
