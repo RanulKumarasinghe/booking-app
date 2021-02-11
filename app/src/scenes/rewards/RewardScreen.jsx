de51e8f7b2e2bd3fe7258d87d4d5c17a0403c28b
Laurence Barnes <lb569@kent.ac.uk>
2021-02-11 18:42:47 +0000

    Profile screen now has a rewards button

--- app/src/scenes/rewards/RewardScreen.jsx
+++ app/src/scenes/rewards/RewardScreen.jsx
@@ -1,149 +1,161 @@
 import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, Button, Text, TextInput, Image, View, StatusBar} from "react-native";
-import { Divider, Icon } from '@ui-kitten/components';
-import firebase from 'src/utils/firebase';
-import Layout from '@/components/atoms/Layout'
+import {
+  SafeAreaView,
+  StyleSheet,
+  Button,
+  Text,
+  TextInput,
+  Image,
+  View,
+  StatusBar,
+} from "react-native";
+import { Divider, Icon, Layout } from "@ui-kitten/components";
+import firebase from "src/utils/firebase";
 
 const RewardScreen = (props) => {
-
   //State holds the money input by user
   const [money, setMoney] = useState(0);
 
   //This func generates the code
   function makeid() {
     var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     for (var i = 0; i < 6; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-      return text;
-  };
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+  }
 
   //The connection to the DB
-  const rewards = firebase.firestore().collection('rewards');
+  const rewards = firebase.firestore().collection("rewards");
 
   //Converts value from String to Int
   const onTextChange = (money) => {
-    var number = parseFloat(money)
+    var number = parseFloat(money);
     setMoney(number);
-  }
- 
+  };
+
   //The info sent to the DB
-  function addPoints(){
-    rewards.add({
-           money: money,
-          points: null,
-     restrauntId: null,
-      customerId: null,
-      employeeId: null,
-       createdAt: new Date(),
-            code: makeid(),
+  function addPoints() {
+    rewards
+      .add({
+        money: money,
+        points: null,
+        restrauntId: null,
+        customerId: null,
+        employeeId: null,
+        createdAt: new Date(),
+        code: makeid(),
         codeUsed: false,
-    })
-    .then(() => {
-      console.log('Points added!');
-      
-    }).catch(function(error) {
-      console.error("There was an error, please try again: ", error);
-       });
+      })
+      .then(() => {
+        console.log("Points added!");
+      })
+      .catch(function (error) {
+        console.error("There was an error, please try again: ", error);
+      });
   }
 
   return (
-    <Layout>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Image
+          style={styles.userImage}
+          source={{
+            uri:
+              "https://i.pinimg.com/originals/0c/3b/3a/0c3b3adb1a7530892e55ef36d3be6cb8.png",
+          }}
+        />
         <View>
-            <Image
-            style={styles.userImage}
-            source={{uri:'https://thumbs.dreamstime.com/z/vector-illustration-isolated-white-background-user-profile-avatar-black-line-icon-user-profile-avatar-black-solid-icon-121102166.jpg'}}
-            />
-          <View>
-              <Text style={styles.font}>Hello Username</Text>
-              {/* /manage state to get username above/ */}
-            </View>
-            <View style={styles.lineThrough}/>
-            <View>
-              <Text style={styles.font}>Your Points:</Text>
-              <Text style={styles.font}></Text>
-            </View>
-          </View>
-
-        <View>
-          <TextInput
-            style={{
-              height: 40,
-              borderColor: 'gray',
-              borderWidth: 1, width:'80%',
-              alignSelf:'center',
-              alignContent:'center'
-            }}
-            value={money}
-            keyboardType="numeric"
-            placeholder='Price of meal here'
-            //onChangeText={onTextChange}
-            onChangeText={(money)=>onTextChange(money)}
-            maxLength={4}
-          />
-          <View style={styles.inputButton}>
-            <Button title="Redeem Points" onPress={addPoints} />
-          </View>
+          <Text style={styles.font}>Hello Username</Text>
+          {/* /manage state to get username above/ */}
         </View>
-    </Layout>
+        <View style={styles.lineThrough} />
+        <View>
+          <Text style={styles.font}>Your Points:</Text>
+          <Text style={styles.font}>£{money}: has been added</Text>
+        </View>
+      </View>
+
+      <View>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: "gray",
+            borderWidth: 1,
+            width: "80%",
+            alignSelf: "center",
+            alignContent: "center",
+          }}
+          value={money}
+          keyboardType="numeric"
+          placeholder="Price of meal here"
+          onChangeText={(money) => onTextChange(money)}
+          maxLength={4}
+        />
+        <View style={styles.inputButton}>
+          <Button title="Redeem Points" onPress={addPoints} />
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
-    alignItems: 'center',
+    alignItems: "center",
   },
 
-  userImage:{
-    width:'40%',
-    height:'20%',
-    alignSelf:'center',
+  userImage: {
+    width: "40%",
+    height: "60%",
+    alignSelf: "center",
   },
 
-  lineThrough:{
-    borderBottomColor: 'black',
+  lineThrough: {
+    borderBottomColor: "black",
     borderBottomWidth: 1,
   },
 
-  container:{
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  container: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     // paddingTop:'12%'
   },
 
-  header:{
-    alignContent:'center',
-    backgroundColor:'green',
+  header: {
+    alignContent: "center",
+    backgroundColor: "green",
     // paddingTop:30,
   },
 
-  font:{
-    alignSelf:'center',
-    fontSize:18,
+  font: {
+    alignSelf: "center",
+    fontSize: 18,
   },
 
-  button:{
-    marginBottom:10,
-    borderRadius:50,
-    width:'80%',
-    alignSelf:'center',
+  button: {
+    marginBottom: 10,
+    borderRadius: 50,
+    width: "80%",
+    alignSelf: "center",
   },
 
-  input:{
-    flex:1,
-    flexDirection:'row',
-    width:'50%',
-    height:25,
-    paddingLeft:'10%',
+  input: {
+    flex: 1,
+    flexDirection: "row",
+    width: "50%",
+    height: 25,
+    paddingLeft: "10%",
   },
 
-  inputButton:{
-    marginBottom:10,
-    borderRadius:50,
-    width:'80%',
-    alignSelf:'center',
-    height:'50%',
+  inputButton: {
+    marginBottom: 10,
+    borderRadius: 50,
+    width: "80%",
+    alignSelf: "center",
+    height: "50%",
   },
 });
 
