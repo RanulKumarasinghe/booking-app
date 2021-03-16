cb733360a6720ac499ed9662eeca88b66b9f9f67
Ranul <rk303@kent.ac.uk>
2021-03-16 16:58:39 +0000

    Restaurant Screen changes to Menu and a host of bug fixes

--- app/src/components/Menu/MenuItem.jsx
+++ app/src/components/Menu/MenuItem.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
-import { SafeAreaView, TouchableHighlight, View, Image, StyleSheet } from 'react-native';
+import { SafeAreaView, TouchableHighlight, View, Image, StyleSheet, ImageBackground} from 'react-native';
 import { Text, Divider, Button, Drawer, DrawerGroup, DrawerItem } from '@ui-kitten/components';
+import { useSelector } from "react-redux";
 
 //Formats text for the drawer
 const MenuItem = (props) => {
+  const restaurantManager = useSelector(state => state.staffRestaurant.restaurant);
+  // console.log(restaurantManager)
+  let disabled = false;
+  if (restaurantManager != null) {
+    disabled = true;
+  }
   return (
     <>
-    <TouchableHighlight style={styles.item} key={props.id} onPress={props.onPress} >
+    <TouchableHighlight disabled={disabled} style={styles.item} key={props.id} onPress={props.onPress} >
       <>
-        <View>
+        <View style={styles.listRow}>
           <Text category='p1'>{props.name}</Text>
+          <ImageBackground
+              source={{ uri: props?.imageUrl }}
+              style={styles.bgImage}
+            >
+            </ImageBackground>
         </View>
         <View >
+        <View style={styles.listRow}>
           <Text style={{width: 300 }} category='p2'>{props.desc}</Text>
+          </View>
           <View style={styles.listRow}>
-          <Text style={{width: 300 }} category='p2'>{props.price}</Text>
+          <Text style={{width: 300 }} category='p2'>{`£${props.price}`}</Text>
           <Text style={{width: 300 }} category='p2'>{props.type}</Text>
           </View>
         </View>
@@ -29,7 +43,7 @@ const MenuItem = (props) => {
 const styles = StyleSheet.create({
   item: {
     padding: 10,
-    backgroundColor: '#86ffc8',
+    backgroundColor: '#34ebe8',
   },
   image: {
     width: '10%',
@@ -37,8 +51,13 @@ const styles = StyleSheet.create({
     marginTop: 15
   },
   listRow: {
-    flexDirection: 'row'
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
+  bgImage: {
+    width: 40,
+    height: 40
+  }
 });
 
 export default MenuItem
