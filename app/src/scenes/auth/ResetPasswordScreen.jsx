ef3f0ffb88cba81915258385bfc8fc763171acbc
MINVY-779VKA49\Bengel <minderrx2@gmail.com>
2021-03-19 15:01:45 +0000

    Modal

--- app/src/scenes/auth/ResetPasswordScreen.jsx
+++ app/src/scenes/auth/ResetPasswordScreen.jsx
@@ -12,18 +12,21 @@ import firebase from "src/utils/firebase";
 
 const ResetPasswordScreen = (props) => {
   const [newPassword, setNewPassword] = useState("");
-
   const [visible, setVisible] = useState(false);
+  const [passwordChanged, setPasswordChanged] = useState(false);
 
   const handleChangePassword = () => {
     var user = firebase.auth().currentUser;
     user
       .updatePassword(newPassword)
       .then(() => {
+        setPasswordChanged(true);
         setVisible(true);
         console.log("Password was changed");
       })
       .catch((error) => {
+        setPasswordChanged(false);
+        setVisible(true);
         console.log("Password was not changed");
       });
   };
@@ -43,14 +46,16 @@ const ResetPasswordScreen = (props) => {
 
         <Text>Enter an new password and press "Change Password" to update</Text>
 
-        <Button onPress={() => handleChangePassword}>Change Password</Button>
+        <Button onPress={() => handleChangePassword()}>Change Password</Button>
         <Modal
           visible={visible}
           backdropStyle={styles.backdrop}
           onBackdropPress={() => setVisible(false)}
         >
-          <Card disabled={true}>
-            <Text>Password has been changed</Text>
+          <Card disabled={true}
+          //style={{minWidth:400}}
+          >
+            {passwordChanged ? <Text>Password has been changed</Text> : <Text>Password has not been changed</Text>}
             <Button onPress={() => setVisible(false)}>DISMISS</Button>
           </Card>
         </Modal>
