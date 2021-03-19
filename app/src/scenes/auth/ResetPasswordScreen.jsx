cc74715adf8c3446d1d1efa37e372559f62c1b29
Laurence Barnes <lb569@kent.ac.uk>
2021-03-19 09:29:48 +0000

    Update states

--- app/src/scenes/auth/ResetPasswordScreen.jsx
+++ app/src/scenes/auth/ResetPasswordScreen.jsx
@@ -1,19 +1,27 @@
-import React, {useState} from 'react';
-import { SafeAreaView, View, Divider, Icon, Layout, Text,} from 'react-native';
-import { StyleSheet, TextInput, Button } from 'react-native'
+import React, { useState } from "react";
+import { SafeAreaView, View, Divider, Icon, Layout, Text } from "react-native";
+import { StyleSheet, TextInput, Button } from "react-native";
 // import {login } from '@/store/actions/auth'
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
+import firebase from "src/utils/firebase";
 
 const ResetPasswordScreen = (props) => {
-  const [email, setEmail] = useState('')
+  const [newPassword, setNewPassword] = useState("");
 
   const handleResetPassword = () => {
+    var user = firebase.auth().currentUser;
+    user
+      .updatePassword(newPassword)
+      .then(() => {
+        console.log("Password was changed");
+      })
+      .catch((error) => {
+        console.log("Password was not changed");
+      });
+  };
 
-  }
-  
   return (
     <View style={styles.container}>
-
       {/* <Image source={require('_assets/icon.png')} /> */}
 
       <Text>Reset your password</Text>
@@ -21,32 +29,31 @@ const ResetPasswordScreen = (props) => {
       <TextInput
         style={styles.textInput}
         autoCapitalize="none"
-        placeholder="Email"
-        onChangeText={email => setEmail(email)}
-        value={email}
+        placeholder="New Password"
+        onChangeText={(newPassword) => setNewPassword(newPassword)}
+        value={newPassword}
       />
 
-      <Text>By signing up, you agree to our terms of service and privacy policy.</Text>
+      <Text>Enter an new password and press "Reset Password" to update</Text>
 
-      <Button title="Send Email" onPress={handleResetPassword} />
+      <Button title="Reset Password" onPress={handleResetPassword} />
     </View>
   );
 };
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
   textInput: {
     height: 40,
-    width: '90%',
-    borderColor: 'gray',
+    width: "90%",
+    borderColor: "gray",
     borderWidth: 1,
-    marginTop: 8
-  }
-})
+    marginTop: 8,
+  },
+});
 
 export default ResetPasswordScreen;
