8f29dc6310d96a59529ff4ff4a274d989409bcb0
Laurence Barnes <lb569@kent.ac.uk>
2021-03-19 13:38:29 +0000

    Modul update

--- app/src/scenes/auth/ResetPasswordScreen.jsx
+++ app/src/scenes/auth/ResetPasswordScreen.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
-import { Input, Text, Button, Modal, Card } from "@ui-kitten/components";
+import {
+  Input,
+  Text,
+  Button,
+  Layout,
+  Modal,
+  Card,
+} from "@ui-kitten/components";
 import firebase from "src/utils/firebase";
 
 const ResetPasswordScreen = (props) => {
@@ -22,49 +29,48 @@ const ResetPasswordScreen = (props) => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text>Reset your password</Text>
+    <Layout style={styles.container}>
+      <View style={styles.padding}>
+        <Text>Change your password</Text>
 
-      <Input
-        style={styles.textInput}
-        autoCapitalize="none"
-        placeholder="New Password"
-        onChangeText={(newPassword) => setNewPassword(newPassword)}
-        value={newPassword}
-      />
+        <Input
+          style={styles.textInput}
+          autoCapitalize="none"
+          placeholder="New Password"
+          onChangeText={(newPassword) => setNewPassword(newPassword)}
+          value={newPassword}
+        />
 
-      <Text>Enter an new password and press "Change Password" to update</Text>
+        <Text>Enter an new password and press "Change Password" to update</Text>
 
-      <Button onPress={() => handleChangePassword}>Change Password</Button>
-      <Modal
-        visible={visible}
-        backdropStyle={styles.backdrop}
-        onBackdropPress={() => setVisible(false)}
-      >
-        <Card disabled={true}>
-          <Text>Password has been changed</Text>
-          <Button onPress={() => setVisible(false)}>DISMISS</Button>
-        </Card>
-      </Modal>
-    </View>
+        <Button onPress={() => handleChangePassword}>Change Password</Button>
+        <Modal
+          visible={visible}
+          backdropStyle={styles.backdrop}
+          onBackdropPress={() => setVisible(false)}
+        >
+          <Card disabled={true}>
+            <Text>Password has been changed</Text>
+            <Button onPress={() => setVisible(false)}>DISMISS</Button>
+          </Card>
+        </Modal>
+      </View>
+    </Layout>
   );
 };
 
 const styles = StyleSheet.create({
+  padding: {
+    padding: 30,
+  },
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     textAlign: "center",
-    margin: 20,
-    padding: 10,
-    minHeight: 192,
   },
   button: {
     minWidth: "90%",
-    marginTop: 15,
-    margin: 10,
-    borderRadius: 100,
   },
   backdrop: {
     backgroundColor: "rgba(0, 0, 0, 0.5)",
